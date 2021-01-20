e053bbe3c407a252a8091441a1198e602e3c0c36
abersheeran <me@abersheeran.com>
2021-01-20 23:21:52 +0800

    增加 serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './pure.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker.js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
