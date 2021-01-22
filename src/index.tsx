15014e74e852401f8e726d941cf6a3519c85f144
abersheeran <me@abersheeran.com>
2021-01-22 23:39:03 +0800

    增加l serviceWorker.ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './pure.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
