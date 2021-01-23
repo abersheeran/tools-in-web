c4c3e5804c07db9be5fa9ba8c8bd14999546e24a
abersheeran <me@abersheeran.com>
2021-01-23 12:29:09 +0800

    使用 pwa-typescript 模板重建

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './pure.min.css';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import registerServiceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +13,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
